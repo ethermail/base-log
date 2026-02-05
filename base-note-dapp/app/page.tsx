c0d23d7920aaa478a5159e55fb64d37eb29301e4
ethermail <ethermail@users.noreply.github.com>
2026-02-05 21:31:15 +0800

    refactor: simplify page now that NoteViewer is live

--- base-note-dapp/app/page.tsx
+++ base-note-dapp/app/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { useState } from "react";
 import { ConnectWallet } from "../src/components/ConnectWallet";
 import { NoteViewer } from "../src/components/NoteViewer";
 import { NoteEditor } from "../src/components/NoteEditor";
 
 export default function Page() {
-  const [refreshKey, setRefreshKey] = useState(0);
-
   return (
     <main className="p-6 space-y-4">
       <ConnectWallet />
       <h1 className="text-xl font-semibold">Base Note DApp</h1>
-      <NoteViewer refreshKey={refreshKey} />
-      <NoteEditor onSaved={() => setRefreshKey((k) => k + 1)} />
+      <NoteViewer />
+      <NoteEditor />
     </main>
   );
 }
