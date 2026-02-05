f8e95ef054281094352ae569894f159b37183539
ethermail <ethermail@users.noreply.github.com>
2026-02-05 21:13:36 +0800

    feat: render NoteViewer on home page

--- base-note-dapp/app/page.tsx
+++ base-note-dapp/app/page.tsx
@@ -1,11 +1,12 @@
-import { ConnectWallet } from "@/src/components/ConnectWallet";
+import { ConnectWallet } from "../src/components/ConnectWallet";
+import { NoteViewer } from "../src/components/NoteViewer";
 
 export default function Page() {
   return (
     <main className="p-6 space-y-4">
       <ConnectWallet />
       <h1 className="text-xl font-semibold">Base Note DApp</h1>
+      <NoteViewer />
     </main>
   );
 }
-
